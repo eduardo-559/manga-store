0fc1855baa80d1c48453936f2c22159dd872fd5e
Eduardo de Oliveira Sousa <84216150+eduardo-559@users.noreply.github.com>
2024-03-29 23:01:47 -0300

    Add files via upload

---
+++ backend/src/test/java/br/quixada/ufc/mangastore/MangaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.quixada.ufc.mangastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
